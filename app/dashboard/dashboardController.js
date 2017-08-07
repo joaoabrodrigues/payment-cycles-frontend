bc594e501d302a44ceeffa177a082220afb76e61
joaoabrodrigues <joaoabrodrigues@outlook.com>
2017-08-07 20:42:36 -0300

    Fixing unexpected logout

--- app/dashboard/dashboardController.js
+++ app/dashboard/dashboardController.js
@@ -9,7 +9,7 @@
     const vm = this
     vm.getSummary = function() {
       const url = 'https://payment-cycles.herokuapp.com/api/v1/paymentSummary'
-      $http.headers.userEmail = JSON.parse(localStorage.getItem(consts.userKey)).email
+      $http.defaults.headers.common.userEmail = JSON.parse(localStorage.getItem(consts.userKey)).email
       $http.defaults.headers.common.Authorization = JSON.parse(localStorage.getItem(consts.userKey)).token
       $http.get(url).then(function(response){
         const {credit = 0, debt = 0} = response.data
